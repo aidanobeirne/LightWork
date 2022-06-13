a86d035e4d7d695858975de41edf32660bf0f95f
aidanobeirne <aidano@stanford.edu>
2022-06-13 15:09:01 -0700

    Making the package installable on lab computers

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(
    description='Generalized code to run high-dimensional data scans using optics instrumentation',
    author='Aidan OBeirne',
    author_email='aidanobeirne@me.com',
+   url='https://github.com/aidanobeirne/SimpleScan.git',
    packages=['SimpleScan'],  #same as name
-   install_requires=[], #external packages as dependencies
+   install_requires=['PyMeasure', 'socket', 'PyVISA', 'signal'], #external packages as dependencies
+   include_package_data=True,
+   scripts=[
+            'scripts/cool',
+            'scripts/skype',
+           ]
 )
