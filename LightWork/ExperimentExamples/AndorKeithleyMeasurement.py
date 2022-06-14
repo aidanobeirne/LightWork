90eaefb05b6f49fd4f7e37f8e0d1fcdc7dcf213c
aidanobeirne <aidano@stanford.edu>
2022-06-13 21:56:37 -0700

    Changes to measurement examples

--- LightWork/ExperimentExamples/AndorKeithleyMeasurement.py
+++ LightWork/ExperimentExamples/AndorKeithleyMeasurement.py
@@ -1,27 +1,21 @@
 import numpy as np
-
-from LightWork.MeasurementObjects.JobinYvonMeasurementObject import JobinYvonMeasurementObject
+from datetime import date
+from LightWork.MeasurementObjects.AndorMeasurementObject import AndorMeasurementObject
 from LightWork.ScanObjects.KeithleyScanObject import KeithleyScanObject
 from LightWork.SimpleScan import SimpleScan
-cam = JobinYvonMeasurementObject(exposure_in_s=1, grating=1, use_synapse=1, numavgs=1, center_wl =700, ystart=125, yend=165, slitwidth_mm=1.0)
+
+cam = AndorMeasurementObject(exposure_in_s=1, vcent=135, vheight=21, AcquisitionMode=1, Readmode=3, temperature=-89, wait_to_cool=False, path_to_domain=None)
 topgate = KeithleyScanObject(address ='58', name='TG', compliance_current=300e-9, scan_values=np.arange(-1, 1.25, 0.25), scan_nest_index=0)
 backgate = KeithleyScanObject(address ='59', name='BG', compliance_current=300e-9, scan_values=np.arange(-1, 1.25, 0.25), scan_nest_index=0)
 
 
 Measurement = SimpleScan(
      measurement_instrument=cam, scan_instruments=[topgate, backgate], laser_shutter=False,
-     savepath=r"C:\Users\heinz\Documents\User Files\aidan\simpleSweeptest\{}/".format(date.today().strftime("%b-%d-%Y")),
+     savepath=r"C:\Users\HeinzLab\Documents\Aidan\Test\{}/".format(date.today().strftime("%b-%d-%Y")),
      savename='test',scan_notes='', save_npz=0, notify_me=False
      )
 
 Measurement.run_sweep()
-    
-    
-
-
-
-
-
 
 
 
