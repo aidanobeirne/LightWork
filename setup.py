959fa8a1cea1de29fd342116763ece2fa84700a2
aidanobeirne <aidano@stanford.edu>
2022-06-21 15:20:30 -0700

    added pythonnet

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='LightWork',
-   version='1.0',
-   description='Generalized code to run high-dimensional data scans using optics instrumentation',
-   author='Aidan OBeirne',
-   author_email='aidanobeirne@me.com',
-   url='https://github.com/aidanobeirne/LightWork.git',
-   packages=find_packages(),  
-   include_package_data=True,
-   install_requires=['PyMeasure', 'PyVISA', 'twilio']#, 'PyQt5', 'pyqtgraph'], #external packages as dependencies
+    name='LightWork',
+    version='1.0',
+    description='Generalized code to run high-dimensional data scans using optics instrumentation',
+    author='Aidan OBeirne, Jenny Hu',
+    author_email='aidanobeirne@me.com',
+    url='https://github.com/aidanobeirne/LightWork.git',
+    packages=find_packages(),
+    include_package_data=True,
+    # , 'PyQt5', 'pyqtgraph'], #external packages as dependencies
+    install_requires=['PyMeasure', 'PyVISA', 'twilio', 'nidaqmx', 'pythonnet']
 )
